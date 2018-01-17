c4ae25176f9b529b3c4447c9b1f6563a5c81c6a9
Mason Kriegel <mason.kriegel@honeywell.com>
2018-01-17 10:04:32 -0600

    Basic Services and code clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,22 +12,26 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    DishdetailComponent
-  ],
-  imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      FormsModule,
-      HttpModule,
-      MaterialModule,
-      FlexLayoutModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        DishdetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpModule,
+        MaterialModule,
+        FlexLayoutModule,
+    ],
+    providers: [
+        DishService
+    ],
+
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
