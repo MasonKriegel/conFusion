6064b88ee131da2d69da2b42dce08cd2d016ac32
Mason Kriegel <mason.kriegel@honeywell.com>
2018-02-04 18:33:36 -0600

    Promises part 2

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -14,6 +14,7 @@ import {
     MatSlideToggleModule,
     MatSelectModule,
     MatFormFieldModule,
+    MatProgressSpinnerModule,
 } from '@angular/material';
 
 @NgModule({
@@ -31,6 +32,7 @@ import {
         MatSlideToggleModule,
         MatSelectModule,
         MatFormFieldModule,
+        MatProgressSpinnerModule,
     ],
     exports: [
         MatButtonModule,
@@ -46,6 +48,7 @@ import {
         MatSlideToggleModule,
         MatSelectModule,
         MatFormFieldModule,
+        MatProgressSpinnerModule,
     ]
 })
 export class MaterialModule { }
